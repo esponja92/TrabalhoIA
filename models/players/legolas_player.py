166184e066fb2994f09f4d61bb7b54c449d35561
unknown <victor.peres93@gmail.com>
2015-06-19 20:26:24 -0300

    Heurística no Legolas: - Implementação de heurísticas de score, de movimentos válidos e de canto - Cada heurística tem seu determinado peso - Ainda não se sabe qual é o melhor peso para cada heurística

--- models/players/legolas_player.py
+++ models/players/legolas_player.py
@@ -6,7 +6,9 @@ class Legolas:
     import random        
 
     def play(self, board):
-        return self.minimax(self.MAX_DEPTH, -float("inf"), +float("inf"), board, self.color)[0]
+        ret = self.minimax(self.MAX_DEPTH, -float("inf"), +float("inf"), board, self.color)
+        #print ret[1]
+        return ret[0]
 
     def minimax(self, depth, alpha, beta, board, playerColor):
         bestMove = 0
@@ -58,7 +60,43 @@ class Legolas:
             return [bestMove, beta]
 
     def getScore(self, board):
+        totalScore = 0
+        totalScore += 25 * ((self.getPlayerScore(board) - self.getOpponentScore(board)) / float((self.getPlayerScore(board) + self.getOpponentScore(board))))
+
+        playerValidMoves = board.valid_moves(self.color)
+        opponentValidMoves = board.valid_moves(board._opponent(self.color))
+        if (len(playerValidMoves) + len(opponentValidMoves) != 0):
+            totalScore += 50 * ((len(playerValidMoves) - len(opponentValidMoves)) / float((len(playerValidMoves) + len(opponentValidMoves))))
+
+        playerCorners = self.getNOfCorners(board, self.color)
+        opponentCorners = self.getNOfCorners(board, board._opponent(self.color))
+        if (playerCorners + opponentCorners != 0):
+            totalScore += 30 * ((playerCorners - opponentCorners)/float((playerCorners + opponentCorners)))
+
+        #print totalScore
+        return totalScore
+
+
+    def getPlayerScore(self, board):
         if (self.color == board.WHITE):
             return board.score()[0]
         else:
             return board.score()[1]
+            
+    def getOpponentScore(self, board):
+        if (self.color == board.WHITE):
+            return board.score()[1]
+        else:
+            return board.score()[0]
+
+    def getNOfCorners(self, board, color):
+        nOfCorners = 0
+        if (board.board[1][1] == self.color):
+            nOfCorners += 1
+        if (board.board[1][8] == self.color):
+            nOfCorners += 1
+        if (board.board[8][1] == self.color):
+            nOfCorners += 1
+        if (board.board[8][8] == self.color):
+            nOfCorners += 1
+        return nOfCorners
